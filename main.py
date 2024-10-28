f8dc8f6fc81d36c85e1b2a2feaec296beb7d7ff6
Peter <peter279k@gmail.com>
2024-10-29 01:50:02 +0800

    Replace json with the string approach

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import time
 import config
 from microbit import *
@@ -8,8 +7,8 @@ from version_2 import DHT11
 uart.init(config.uart_rate)
 while True:
     sensor_info = {
-        'temp_control': 0.0,
-        'humidity_control': 0.0,
+        'temp_control': None,
+        'humidity_control': None,
     }
 
     try:
@@ -19,9 +18,9 @@ while True:
         sensor_info['humidity_control'] = humidity
 
         print(sensor_info)
-        sensor_str = json.dumps(sensor_info)
-        uart.write(sensor_str.encode('utf-8'))
-
         time.sleep(2)
     except:
         time.sleep(3)
+    finally:
+        sensor_str = '{},{}'.format(sensor_info['temp_control'], sensor_info['humidity_control'])
+        uart.write(sensor_str.encode('utf-8'))
